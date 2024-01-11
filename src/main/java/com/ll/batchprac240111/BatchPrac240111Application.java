8de3191a07a2740f655b84103d08a7957c5ca2f9
harriet221 <potter19@dongguk.edu>
2024-01-11 13:55:10 +0900

    prac

---
+++ src/main/java/com/ll/batchprac240111/BatchPrac240111Application.java
@@ -0,0 +1,12 @@
+package com.ll.batchprac240111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchPrac240111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchPrac240111Application.class, args);
+    }
+}
---
+++ src/test/java/com/ll/batchprac240111/BatchPrac240111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.batchprac240111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchPrac240111ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
